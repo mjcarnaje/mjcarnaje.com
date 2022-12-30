c84927b5f8b9d364b5e44b85bbf105e45a139d00
Michael James Carnaje <michaeljamescarnaje1@gmail.com>
2022-12-30 14:01:09 +0800

    chore: clean up

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Index = () => (
-  <div className="flex justify-center items-center w-screen h-screen">
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
-  </div>
-);
-
-export default Index;
