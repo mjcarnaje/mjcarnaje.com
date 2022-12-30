4405d0a593082d73d1e5dc5e933a4d4a6b00217b
Michael James Carnaje <michaeljamescarnaje1@gmail.com>
2022-12-30 12:14:28 +0800

    chore: chakra ui to tailwindcss

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Index = () => (
+  <div className="flex justify-center items-center w-screen h-screen">
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  </div>
+);
+
+export default Index;
