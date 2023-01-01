5b34c6ed2794b4190cf2306e72dbc4200fd4f81b
Michael James Carnaje <michaeljamescarnaje1@gmail.com>
2023-01-01 21:30:10 +0800

    chore: add BASE_URL in env

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,1 @@
-export const BASE_URL = "http://localhost:3000";
-// export const BASE_URL =
-//   process.env.NODE_ENV === "development"
-//     ? "http://localhost:3000"
-//     : "https://mjcarnaje.vercel.app";
+export const BASE_URL = process.env.BASE_URL;
