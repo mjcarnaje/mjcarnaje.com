b7d39829795dc6dd0b408edfb4fce2eba8234dc8
Michael James Carnaje <michaeljamescarnaje1@gmail.com>
2023-01-01 21:53:55 +0800

    fix: direct request instead from api

--- app/spotify/page.tsx
+++ app/spotify/page.tsx
@@ -2,23 +2,63 @@ import { Container, TopArtists, TopTracks } from "@components";
 import { CurrentlyPlaying } from "@components/spotify/CurrentlyPlaying";
 import { Profile } from "@components/spotify/Profile";
 import { BASE_URL } from "@constants";
+import { getAccessToken } from "lib/spotify";
 
 async function getTopArtists(): Promise<ArtistReponse[]> {
-  const response = await fetch(`${BASE_URL}/api/spotify/top-artists`);
+  const access_token = await getAccessToken();
+  const response = await fetch("https://api.spotify.com/v1/me/top/artists", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
   if (!response.ok) {
     throw new Error(`Failed to fetch data ${BASE_URL}/api/spotify/top-artists`);
   }
-  return response.json();
+  const data: SpotifyApi.UsersTopArtistsResponse = await response.json();
+
+  const artists: ArtistReponse[] = data.items.map((artist) => ({
+    id: artist.id,
+    name: artist.name,
+    image_url: artist.images[0].url,
+    uri: artist.uri,
+    followers_count: artist.followers.total,
+  }));
+
+  return artists;
 }
 async function getTopTracks(): Promise<TrackReponse[]> {
-  const response = await fetch(`${BASE_URL}/api/spotify/top-tracks`);
+  const access_token = await getAccessToken();
+  const response = await fetch("https://api.spotify.com/v1/me/top/tracks", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
   if (!response.ok) {
     throw new Error(`Failed to fetch data ${BASE_URL}/api/spotify/top-tracks`);
   }
-  return response.json();
+  const data: SpotifyApi.UsersTopTracksResponse = await response.json();
+  const tracks: TrackReponse[] = data.items.map((track) => ({
+    id: track.id,
+    title: track.name,
+    artist: track.artists.map((artist) => artist.name).join(", "),
+    url: track.external_urls.spotify,
+    image_url: track.album.images[0].url,
+    duration: track.duration_ms,
+    album: track.album.name,
+    uri: track.uri,
+  }));
+  return tracks;
 }
 async function getMe(): Promise<SpotifyApi.UserProfileResponse> {
-  const response = await fetch(`${BASE_URL}/api/spotify/me`);
+  const access_token = await getAccessToken();
+  const response = await fetch("https://api.spotify.com/v1/me", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
   if (!response.ok) {
     throw new Error(`Failed to fetch data ${BASE_URL}/api/spotify/me`);
   }
@@ -27,7 +67,16 @@ async function getMe(): Promise<SpotifyApi.UserProfileResponse> {
 async function getCurrentlyPlaying(): Promise<
   SpotifyApi.CurrentlyPlayingResponse | { is_playing: false }
 > {
-  const response = await fetch(`${BASE_URL}/api/spotify/currently-playing`);
+  const access_token = await getAccessToken();
+  const response = await fetch(
+    "https://api.spotify.com/v1/me/player/currently-playing",
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    }
+  );
   if (!response.ok) {
     throw new Error(
       `Failed to fetch data ${BASE_URL}/api/spotify/currently-playing`
