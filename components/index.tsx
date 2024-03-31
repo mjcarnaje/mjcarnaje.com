d124c43d77a7ec323f091138d2d2cde6eef92ef4
mjcarnaje <michaeljamescarnaje1@gmail.com>
2024-03-31 19:07:13 +0800

    fix: remove undefind imports

--- components/index.tsx
+++ components/index.tsx
@@ -1,7 +1,5 @@
 export { Container } from "./DefaultContainer";
 export { DefaultTags } from "./DefaultTags";
 export { SocialLink } from "./SocialLink";
-export { TopTracks } from "./spotify/TopTracks";
-export { TopArtists } from "./spotify/TopArtists";
 export { NavItem } from "./nav/NavItem";
 export { Navbar } from "./nav/Navbar";
